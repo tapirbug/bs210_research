7098960338eb65d5ecf6ba4486f5456561b19bad
Philipp Stadler <hello@phstadler.com>
2021-08-18 15:33:48 +0200

    feat: configurable cycle time for cycle.bat

--- scripts/cycle.bat
+++ scripts/cycle.bat
@@ -1,5 +1,12 @@
+IF "%1"=="" (
+    SET sleep_secs=5
+)
+IF NOT "%1"=="" (
+    SET sleep_secs="%1"
+)
+
 :start
 :: Loop through destination texts 0 through 6 (inclusive), step size 1,
 :: Set the text for each number, then sleep 5 seconds
-FOR /L %%i IN (0,1,6) DO CALL destination %%i & sleep 5
+FOR /L %%i IN (0,1,6) DO CALL destination %%i & sleep %sleep_secs%
 goto start
