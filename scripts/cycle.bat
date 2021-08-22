e49b1199fa17f340bce2dd05250004222a5295a6
Philipp Stadler <hello@phstadler.com>
2021-08-22 18:51:23 +0200

    feat: configurable destination count in cycle script
    `cycle.bat` now accepts the index of the last destination to loop through, always starting with destination zero. The first argument is still the switch time.

--- scripts/cycle.bat
+++ scripts/cycle.bat
@@ -1,12 +1,19 @@
 IF "%1"=="" (
-    SET sleep_secs=5
+    SET /a sleep_secs=5
 )
 IF NOT "%1"=="" (
-    SET sleep_secs="%1"
+    SET /a sleep_secs="%1"
+)
+
+IF "%2"=="" (
+    SET /a destinations=1
+)
+IF NOT "%2"=="" (
+    SET /a destinations="%2"
 )
 
 :start
-:: Loop through destination texts 0 through 6 (inclusive), step size 1,
+:: Loop through destination texts 0, step size 1, up to variable destination given with second argument (inclusive)
 :: Set the text for each number, then sleep 5 seconds
-FOR /L %%i IN (0,1,6) DO CALL destination %%i & sleep %sleep_secs%
+FOR /L %%i IN (0,1,%destinations%) DO CALL destination %%i & sleep %sleep_secs%
 goto start
